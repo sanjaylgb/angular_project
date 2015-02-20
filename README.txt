0663b98a6de8fb3c605314975543534008804512
Sanjay Bhavnani <sanjaylgb@yahoo.co.in>
2015-02-20 09:27:29 -0800

    build.sh changes and Read me file
    build.sh changes and Read me file

---
+++ README.txt
@@ -0,0 +1,3 @@
+-Need Tomcat and Maven to run this project
+-Place tomcat home path in the build.sh script
+
--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ TOMCAT_HOME="/Users/sanjay/Desktop/apache-tomcat-7.0.53/"
 mvn clean install
 mv target/PhoneBookWebApp-1.0-SNAPSHOT.war $TOMCAT_HOME/webapps/phonebook.war
 sh $TOMCAT_HOME/bin/startup.sh
-echo "Launch the application at: http://localhost:8080/phonebook"
+echo "*****************************Launch the application at: http://localhost:8080/phonebook"
+
